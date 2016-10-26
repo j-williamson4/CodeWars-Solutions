63eceff820a03df172aa23c034606eccd2af518b
John <jhw48@txstate.edu>
2016-10-26 14:09:04 -0500

    first commit

---
+++ 7kyu/findNextSquare.js
@@ -0,0 +1,13 @@
+function findNextSquare(sq) {
+ // Return the next square if sq is a perfect square, -1 otherwise
+ var x = isSquare(sq);
+ if(x === false) return -1;
+ else {
+  var perfectSquare = Math.sqrt(sq);
+  var nextSquare = Math.pow((perfectSquare+1),2);
+  return nextSquare;
+ }
+}
+
+var isSquare = function(n) {
+  return n > 0 && Math.sqrt(n) % 1 === 0;
